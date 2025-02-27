e92fcefd232dbbd133f75da4323d53a8474a779f
Steven Moyer <steven.moyer416@gmail.com>
2025-02-26 21:16:50 -0500

    resource2

--- Terence_app1.py
+++ Terence_app1.py
@@ -327,7 +327,7 @@ def index():
 
 @app.route('/resource')
 def about_terence():
-    return render_template(get_template("resouce"))
+    return render_template(get_template("resource"))
 
 @app.route('/audio_player')
 def audio_player():
